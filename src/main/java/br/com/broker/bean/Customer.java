124d6726cd788b2c19d1ff04b43e0e6d9f481996
Helder Vieira <heldervieira_datum@getnet.com.br>
2021-02-27 23:14:25 -0300

    Ajustes na Aplicação para Produzir Mensagem na Fila do RabbitMQ.

--- src/main/java/br/com/broker/bean/Customer.java
+++ src/main/java/br/com/broker/bean/Customer.java
@@ -1,11 +1,20 @@
 package br.com.broker.bean;
 
 import java.io.Serializable;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class Customer implements Serializable {
 
 	private static final long serialVersionUID = -2986254117985413841L;
@@ -17,6 +26,10 @@ public class Customer implements Serializable {
 	private String endereco;
 	
 	private String sexo;
-
+	
+	@Override
+	public String toString() {
+		return super.toString();
+	}
 }
 
